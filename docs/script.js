5db15092150709edd1a838b578cacc98b3b84dab
Deekshith Muthoju <muthojudeekshith@gmail.com>
2025-12-19 23:18:35 +0530

    Add event listeners for screen navigation

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  function showScreen(id) {
+    document.querySelectorAll(".screen")
+      .forEach(s => s.classList.remove("active"));
+    document.getElementById(id).classList.add("active");
+  }
+
+  // Interface 1
+  document.getElementById("enterBtn").onclick = () => {
+    showScreen("screen2");
+  };
+
+  // Interface 2
+  document.getElementById("passBtn").onclick = () => {
+    if (document.getElementById("password").value === "c5d9") {
+      showScreen("screen3");
+    }
+  };
+
+  // Interface 3
+  document.getElementById("warning").onclick = () => {
+    showScreen("screen4");
+  };
+
+});
 
